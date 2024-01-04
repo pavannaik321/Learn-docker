5331af5760695dc31804c1a72eccd12b31a41656
Pavan Naik <82699802+pavannaik321@users.noreply.github.com>
2024-01-04 22:35:49 +0530

    Node container

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.json({
+    "hey":"NodeJs"
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
